ca100496d4a385d49305d96eac828a55cbacbb4f
Administrator <Administrator@UZEO59DVKJCDMSW>
2017-08-02 20:31:54 +0800

    ss

---
+++ src/main/java/dudu/jingjing/App.java
@@ -0,0 +1,13 @@
+package dudu.jingjing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
